2ee9ba4e67fe988f68725af0dbff02afce68089d
Sagar Gonjare <sagargonjare2004@gmail.com>
2025-09-10 00:37:31 +0530

    feat: Impl label encoding data processing technique

---
+++ 1.1.SupervisedLearning/1.RegressionAnalysis/7.DataPreparationExamples/LabelEncoding/With LabelEncoding/label_encoding.py
@@ -0,0 +1,18 @@
+import pandas as pd 
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder
+
+df = pd.read_csv('salary_data.csv')
+
+encoder = LabelEncoder()
+df['Title'] = encoder.fit_transform(df['Title'])
+
+x = df.drop(['Salary'] , axis =1 )
+y = df['Salary']
+
+model = LinearRegression()
+model.fit(x , y)
+
+result = model.predict(pd.DataFrame([[1,2]] , columns = ['Title' , 'Experience']))
+print(result)
+ 
